1bc8648627a50d2dd000ebb322814d67de714b87
besselsm <besselsm@gmail.com>
2016-10-16 17:33:37 +0200

    Update and rename Node.js to Main.js

--- Node.js
+++
@@ -1,7 +0,0 @@
-app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === 'ThisIsMyTestToken') {
-      res.send(req.query['hub.challenge']);
-    } else {
-      res.send('Error, wrong validation token');    
-    }
-  });
